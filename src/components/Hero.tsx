f8188855cbfe482d8ca86c9a5c1e51fe0159894f
MowlandCodes <mowlandgaming@gmail.com>
2025-12-29 12:52:12 +0700

    tweak for the gradient in the hero section

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -11,17 +11,17 @@ export default function Hero() {
 
       <div className="absolute inset-0 z-10 bg-slate-900/70" />
 
-      <div className="absolute bottom-0 left-0 right-0 h-32 bg-linear-to-t from-slate-50 to-transparent z-10" />
+      <div className="absolute bottom-0 left-0 right-0 h-150 bg-linear-to-t from-slate-50/50 to-transparent z-10" />
 
       <div className="relative z-20 text-center px-4 max-w-5xl mx-auto mt-16">
-        <div className="inline-flex items-center gap-2 py-2 px-4 rounded-full bg-white/10 backdrop-blur-md border border-white/20 text-white text-sm font-medium mb-8 shadow-2xl hover:bg-white/20 transition-all cursor-default">
+        <div className="inline-flex items-center gap-2 py-2 px-4 rounded-full bg-white/10 backdrop-blur-sm border border-white/20 text-white text-sm font-medium mb-8 shadow-2xl hover:bg-white/20 transition-all cursor-default">
           <LuMapPin className="text-emerald-400 drop-shadow-md" />
           <span className="tracking-wide">Desa Wisata, Indonesia</span>
         </div>
 
         <h1 className="text-5xl md:text-7xl font-black text-white tracking-tight mb-6 leading-tight drop-shadow-xl">
           Kembali ke Alam, <br />
-          <span className="text-transparent bg-clip-text bg-linear-to-r from-emerald-300 to-teal-200">
+          <span className="text-transparent bg-clip-text bg-linear-to-tr from-emerald-600 to-teal-200">
             Temukan Ketenangan.
           </span>
         </h1>
@@ -32,20 +32,13 @@ export default function Hero() {
         </p>
 
         <div className="flex flex-col sm:flex-row gap-4 justify-center items-center">
-          <button
-            type="button"
-            className="group px-8 py-4 text-base font-bold text-white bg-emerald-600 rounded-full hover:bg-emerald-700 transition-all shadow-lg shadow-emerald-900/20 w-full sm:w-auto flex items-center justify-center gap-2"
+          <a
+            className="group px-8 py-4 text-base font-bold text-white bg-emerald-600 rounded-full hover:bg-emerald-700 transition-all shadow-lg shadow-emerald-900/20 w-full sm:w-auto flex items-center justify-center gap-2 cursor-pointer"
+            href="#"
           >
             Mulai Jelajah
             <LuArrowRight className="group-hover:translate-x-1 transition-transform" />
-          </button>
-
-          <button
-            type="button"
-            className="px-8 py-4 text-base font-bold text-white border border-white/30 bg-white/5 backdrop-blur-sm rounded-full hover:bg-white/10 transition-all w-full sm:w-auto"
-          >
-            Tonton Video
-          </button>
+          </a>
         </div>
       </div>
 
