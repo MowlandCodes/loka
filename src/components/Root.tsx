6029e989e50637eedfa995cd5c4dba7dc25097a4
MowlandCodes <mowlandgaming@gmail.com>
2025-12-29 13:13:48 +0700

    change font to popping

--- src/components/Root.tsx
+++ src/components/Root.tsx
@@ -3,7 +3,7 @@ import Hero from "@/components/Hero";
 
 export default function Root() {
   return (
-    <main className="bg-slate-50 min-h-screen font-inter">
+    <main className="bg-slate-50 min-h-screen font-poppins">
       <Navbar />
       <Hero />
     </main>
